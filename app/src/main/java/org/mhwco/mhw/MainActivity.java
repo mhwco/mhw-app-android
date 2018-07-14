c190bb536f60f079fc12a442956d3af7e56f2db3
CrSjimo <crs_16423@outlook.com>
2018-07-14 23:04:29 +0800

    Unknown bug.Recreate project and initalized

--- app/src/main/java/org/mhwco/mhw/MainActivity.java
+++ app/src/main/java/org/mhwco/mhw/MainActivity.java
@@ -12,98 +12,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.*;
-import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    String VersionName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //FloatingActionButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "测试版应用程序", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        //Drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        //DrawerNavigation
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //WebView
-        WebView wv=findViewById(R.id.wv);
-        /*class WebAppJavaScriptInterface {
-            int CurrentPageRole=0;
-            @JavascriptInterface
-            String getVersionName(){
-                return VersionName;
-            }
-
-            @JavascriptInterface
-            void setCurrentPageRole(int pageRole){
-                CurrentPageRole=pageRole;
-                switch(pageRole){
-                    case 0://homepage
-
-                        break;
-                    case 1://article page
-
-                        break;
-                    case 2://每日一题 page
-
-                        break;
-                    case 3://article list
-
-                        break;
-                    case 4://article list
-
-                        break;
-                    case 5://每日一题 list
-
-                        break;
-                    case 6://about list
-
-                        break;
-                    default://non app page
-
-                        break;
-                }
-            }
-        }
-        WebAppJavaScriptInterface wajsi=new WebAppJavaScriptInterface();
-        wv.addJavascriptInterface(wajsi,"app");*/
-        //WebView WebChromeClient
-        WebChromeClient wvchrome=new WebChromeClient(){
-
-        };
-        //WebView WebViewClient
-        WebViewClient wvclient=new WebViewClient(){
-
-        };
-        //WebView Settings
-        WebSettings wvsettings=wv.getSettings();
-        wvsettings.setJavaScriptEnabled(true);
-        wvsettings.setAllowContentAccess(true);
-        wvsettings.setAllowFileAccess(true);
-        wvsettings.setAllowFileAccessFromFileURLs(true);
-        wvsettings.setDefaultTextEncodingName("utf-8");
-        wvsettings.setSupportZoom(true);
     }
 
     @Override
@@ -131,7 +67,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_refresh) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
@@ -144,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_daily) {
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_article) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_github) {
+        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_about) {
+        } else if (id == R.id.nav_send) {
 
         }
 
